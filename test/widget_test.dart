0fed8b1268e733cef7af39c2438b3aa35ac80169
Atik Islam <atikrights@gmail.com>
2026-04-07 04:53:38 +0600

    fix: update test imports and complete rebranding

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bizos_x_pro/core/app.dart';
+import 'package:ebficBM/core/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
